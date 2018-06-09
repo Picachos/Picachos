1253028286d91e787c364f51a9f71e8e07532eb0
cbuchris <32282177+cbuchris@users.noreply.github.com>
2018-06-09 16:50:55 -0700

    cambio

--- Picachos.Frontend/Vistas/Clientes/ConsultarClientes.aspx.designer.cs
+++ Picachos.Frontend/Vistas/Clientes/ConsultarClientes.aspx.designer.cs
@@ -1,5 +1,10 @@
 ﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
 //
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
 //------------------------------------------------------------------------------
 
 namespace Picachos.Frontend.Vistas.Clientes {
